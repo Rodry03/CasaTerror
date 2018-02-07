c9212a4faa9ecfa293074a68070f30f5bbd9db74
Rodry03 <03.rodry.03@gmail.com>
2018-02-07 13:40:23 +0100

    Creadas clases

---
+++ CasaTerror/src/casaterror/Objeto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casaterror;
+
+/**
+ *
+ * @author rodry
+ */
+public class Objeto {
+    private int estado; // Estado del objeto
+    private String desc1; // Descripción para estado 1
+    private String desc2; // Descripción para estado 2
+    private int hab; // Habitación en la que se encuentra
+    private int lotengo; // Indica si tengo este objeto en mi inventario
+
+}
